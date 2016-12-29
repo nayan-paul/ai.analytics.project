b4fe2f6f78bac8cbb20de3c35c5765c353cfe55d
nayan-paul <nayan.j.paul@gmail.com>
2016-12-28 21:17:47 -0500

    28-dec-16

--- consolidated-aggregation-process.py
+++ consolidated-aggregation-process.py
@@ -13,6 +13,8 @@ import pymysql
 #PYSPARK_PYTHON=/opt/anaconda/bin/python spark-submit --master local --jars /tmp/com.mysql.jdbc_5.1.5.jar consolidated-aggregation-process.py 1
 #PYSPARK_PYTHON=/opt/anaconda/bin/python spark-submit --master yarn-client --driver-memory 8G --driver-cores 4 --num-executors 3 --executor-memory 8G --jars /tmp/com.mysql.jdbc_5.1.5.jar /opt/projects/consolidated-aggregation-process.py 1
 
+#PYSPARK_PYTHON=/opt/anaconda/bin/python spark-submit --master local --jars /tmp/com.mysql.jdbc_5.1.5.jar consolidated-aggregation-process.py 2
+
 #crontab -e
 #*/45 * * * * sh /opt/projects/run_consolidated_s3agg_process.sh >> /opt/projects/run_consolidated_s3agg_process.stdout 2>>/opt/projects/run_consolidated_s3agg_process.stderr
 
@@ -24,6 +26,9 @@ startTimeKey = currentTime.strftime('%Y-%m-%d')
 
 s3_fields=('key_dt','key_type','offer_id','offer_advertiser_id','destination_subid','count_of_clicks')
 s3_agg_dto = namedtuple('s3_agg_dto',s3_fields)
+
+agg_fields = ('key_dt','key_type','received_revenue','paid_revenue','roi','click_count')
+agg_dto=namedtuple('agg_dto',agg_fields)
 ###############################################################################################################
 #M1
 def M1(row):
@@ -49,6 +54,33 @@ def M2(o):
 #def
 #M2
 ################################################################################################################
+#M3
+def M3(row):
+	try:
+		key = row['key_dt'][:10]
+		o= agg_dto(key_dt=key,key_type=row['key_type'],received_revenue=row['received_revenue'],paid_revenue=row['paid_revenue'],roi=row['roi'],click_count=row['click_count'])
+		return (key+"-"+o.key_type,o)
+	#try
+	except:
+		traceback.print_exc()
+	#except
+	finally:
+		print 'end of process'
+	#finally
+###################################################################################################
+#M4
+def M4(o):
+	try:
+		v= o[1]
+		return (v.key_dt,v.key_type,v.received_revenue,v.paid_revenue,v.roi,v.count_of_clicks)
+	#try
+	except:
+		traceback.print_exc()
+	#except
+	finally:
+		print 'end of process'
+	#finally
+###################################################################################################
 if __name__=='__main__':
 	context = SparkContext(appName='Spark consolidated aggregation of redshift tables')
 	sqlContext = SQLContext(context)
@@ -86,10 +118,36 @@ if __name__=='__main__':
 			#for
 			cursor.close()
 			con.commit()
+			con.close()
 			#processedDF = m2RDD.toDF(['key_dt','key_type','offer_id','offer_advertiser_id','destination_subid','count_of_clicks'])	#processedDF.write.jdbc(url='jdbc:mysql://aitracker.c3zkpgahaaif.us-east-1.rds.amazonaws.com:3306/aitracker?user=aitracker&password=aitracker',table='v2_s3_consolidate_stats',mode='append',properties=properties)
 		#if
 		elif sys.argv[1]=='2':
 			print 'process 2'
+			print "(select key_dt,key_type,received_revenue,paid_revenue,roi,click_count from v2_agg_stats where key_dt >= '"+startTimeKey +"' ) "
+			dataFrame = sqlContext.read.format('jdbc').option('url','jdbc:mysql://aitracker.c3zkpgahaaif.us-east-1.rds.amazonaws.com:3306/aitracker?user=aitracker&password=aitracker').option("driver", "com.mysql.jdbc.Driver").option('dbtable',"(select key_dt,key_type,received_revenue,paid_revenue,roi,click_count from v2_agg_stats where key_dt >= '"+startTimeKey +"' )  as custom").load()
+			
+			input = dataFrame.rdd
+			m3RDD = input.map(M3)
+			print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+			print m3RDD.first()
+			def R2(agg,o):
+				try:
+					agg.received_revenue = agg.received_revenue+o.received_revenue
+					agg.paid_revenue=agg.paid_revenue+o.paid_revenue
+					agg.roi = agg.roi+o.roi
+					agg.count_of_clicks = agg.count_of_clicks+o.count_of_clicks
+					
+					return agg
+				#try
+				except:
+					traceback.print_exc()
+				#except
+			r2RDD = m3RDD.reduceByKey(R2)
+			m4RDD = r2RDD.map(M4)
+			
+			for o in m4RDD.collect():
+				print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'+o
+			#for
 		#if
 	#try
 	except:
@@ -97,7 +155,6 @@ if __name__=='__main__':
 	#except
 	finally:
 		context.stop()
-		con.close()
 		print 'end of process...'
 	#finally
 #if
